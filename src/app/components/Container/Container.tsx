34bd727a4e735b96d0f64faa7f9836cc864ea926
Bila Arif <bilalarifhussain@gmail.com>
2024-08-10 02:33:58 +0500

    website logo changed

--- src/app/components/Container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import Box from "@mui/material/Box";
-import React from "react";
-
-const Container = (props: any) => {
-  const { children } = props;
-  return <Box sx={{ maxWidth: { md: 420 } }}>{children}</Box>;
-};
-
-export default Container;
